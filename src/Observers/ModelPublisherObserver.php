408ee1e859cf06b6dce660f0d240f2b4de6abbe8
CodeByKyle <kyle@codebykyle.com>
2020-01-02 17:46:46 +0700

    Upgrade to whole Laravel kit

---
+++ src/Observers/ModelPublisherObserver.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CodeByKyle\LaravelRabbitMqModelPublisher\Observers;
+
+
+class ModelPublisherObserver
+{
+    public function created(Model $model) {
+
+    }
+
+    public function updated($model) {
+
+    }
+
+    public function deleted($model) {
+
+    }
+
+    public function restored($model) {
+
+    }
+
+
+}
