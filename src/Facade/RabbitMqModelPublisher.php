1816bc4ce1740ce9cb5bb27b66970b9739d95322
CodeByKyle <kyle@codebykyle.com>
2020-01-08 18:06:16 +0700

    Update facade namespace

--- src/Facade/RabbitMqModelPublisher.php
+++ src/Facade/RabbitMqModelPublisher.php
@@ -1,7 +1,12 @@
 <?php
 
-namespace CodeByKyle\LaravelRabbitMqModelPublisher;
+namespace CodeByKyle\Facades\LaravelRabbitMqModelPublisher;
 
+/**
+ * Class RabbitMqModelPublisher
+ * @package CodeByKyle\LaravelRabbitMqModelPublisher
+ * @see CodeByKyle\LaravelRabbitMqModelPublisher\RabbitMqModelPublisher
+ */
 class RabbitMqModelPublisher
 {
     protected static function getFacadeAccessor()
